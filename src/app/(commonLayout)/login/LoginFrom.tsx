96412534eb0cd4f9b9b76643de88a9bfb910393f
NurMuhammadCSE <nurmuhammadcse@gmail.com>
2024-09-09 08:10:05 +0600

    React Hook Form Added in Sign Up Form

--- src/app/(commonLayout)/login/LoginFrom.tsx
+++ src/app/(commonLayout)/login/LoginFrom.tsx
@@ -2,37 +2,75 @@
 "use client";
 import { Input } from "@nextui-org/react";
 import Link from "next/link";
-import { createRef, useEffect } from "react";
+import { createRef, useEffect, useState } from "react";
 import { useFormState } from "react-dom";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
 import { loginUser } from "../action/userInfo";
 import ActionSubmitButton from "../components/shared/submitButton/ActionSubmitButton";
+import { EyeSlashFilledIcon } from "./EyeSlashFilledIcon";
+import { EyeFilledIcon } from "./EyeFilledIcon";
 
 export default function LoginFrom() {
   const router = useRouter();
-  const ref = createRef<HTMLFormElement>();
-  const [state, fromAction] = useFormState(loginUser, null);
-  useEffect(() => {
-    if (state && state?.success) {
-      toast.success(state?.message);
-      ref.current?.reset();
-      router.push("/");
-    }
-    if (state && !state?.success) {
-      toast.error(state?.message);
-    }
-  }, [state, ref]);
+  // const ref = createRef<HTMLFormElement>();
+  // const [state, fromAction] = useFormState(loginUser, null);
+  // useEffect(() => {
+  //   if (state && state?.success) {
+  //     toast.success(state?.message);
+  //     ref.current?.reset();
+  //     router.push("/");
+  //   }
+  //   if (state && !state?.success) {
+  //     toast.error(state?.message);
+  //   }
+  // }, [state, ref]);
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => setIsVisible(!isVisible);
+
   return (
     <div>
-      <form ref={ref} action={fromAction}>
-        <Input name="email" type="email" label="Email" variant="bordered" />
-        <Input
+      <form>
+        {/* <Input name="email" type="email" label="Email" variant="bordered" /> */}
+        {/* <Input
           className="mt-3"
           name="password"
           type="password"
           label="Password"
           variant="bordered"
+        /> */}
+        <Input
+          isClearable
+          type="email"
+          label="Email"
+          variant="bordered"
+          placeholder="Enter your email"
+          defaultValue="junior@nextui.org"
+          onClear={() => console.log("input cleared")}
+          className="max-w-xs"
+        />
+        <Input
+          label="Password"
+          variant="bordered"
+          placeholder="Enter your password"
+          endContent={
+            <button
+              className="focus:outline-none"
+              type="button"
+              onClick={toggleVisibility}
+              aria-label="toggle password visibility"
+            >
+              {isVisible ? (
+                <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+              ) : (
+                <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+              )}
+            </button>
+          }
+          type={isVisible ? "text" : "password"}
+          className="max-w-xs mt-3"
         />
         <div className="flex justify-end text-primary">
           <Link href="/register">if you dont have account sign Up</Link>
--- src/app/(commonLayout)/register/signUpFrom.tsx
+++ src/app/(commonLayout)/register/signUpFrom.tsx
@@ -1,40 +1,92 @@
 "use client";
 import { Input } from "@nextui-org/react";
 import Link from "next/link";
-import { createRef, useEffect } from "react";
-import { useFormState } from "react-dom";
 import { toast } from "sonner";
-import { signUpUser } from "../action/userInfo";
 import ActionSubmitButton from "../components/shared/submitButton/ActionSubmitButton";
+import { useSignUpMutation } from "@/redux/api/authApi";
+import { useRouter } from "next/navigation";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 const SignUpFrom = () => {
-  const ref = createRef<HTMLFormElement>();
-  const [state, fromAction] = useFormState(signUpUser, null);
+  const router = useRouter();
 
-  useEffect(() => {
-    if (state && state.success) {
-      toast.success("successfully sign up");
-      ref.current?.reset();
+  const [signUp] = useSignUpMutation();
+  type Inputs = {
+    name: string;
+    email: string;
+    password: string;
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  // TODO: POST http://localhost:5000/api/auth/signUp net::ERR_CONNECTION_REFUSED
+  //! Sign Up Successfully
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    const signUpInfo = {
+      name: data.name,
+      email: data.email,
+      password: data.password,
+    };
+
+    try {
+      const user = await signUp(signUpInfo);
+      const err = user?.error as { data?: { message?: string } };
+
+      if (err?.data?.message === "User already exists") {
+        toast.error("User already exists");
+      } else {
+        toast.success("User Sign Up Successfully");
+        router.push("/login");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong!");
     }
-  }, [state, ref]);
+  };
+
   return (
     <div>
-      <form ref={ref} action={fromAction}>
-        <Input name="name" type="text" label="Name" variant="bordered" />
-        <Input
-          name="email"
-          className="mt-3"
-          type="email"
-          label="Email"
-          variant="bordered"
-        />
-        <Input
-          className="mt-3"
-          type="password"
-          label="Password"
-          name="password"
-          variant="bordered"
-        />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <Input
+            {...register("name", { required: true })}
+            name="name"
+            type="text"
+            label="Name"
+            variant="bordered"
+          />
+          {errors.name && <p className="text-red-500">Name is required</p>}
+        </div>
+        <div>
+          <Input
+            {...register("email", { required: true })}
+            name="email"
+            className="mt-3"
+            type="email"
+            label="Email"
+            variant="bordered"
+          />
+          {errors.email && <p className="text-red-500">Email is required</p>}
+        </div>
+
+        <div>
+          <Input
+            {...register("password", { required: true })}
+            className="mt-3"
+            type="password"
+            label="Password"
+            name="password"
+            variant="bordered"
+          />
+          {errors.password && (
+            <p className="text-red-500">Password is required</p>
+          )}
+        </div>
         <div className="flex justify-end text-primary">
           <Link href="/login">already have account ?</Link>
         </div>
