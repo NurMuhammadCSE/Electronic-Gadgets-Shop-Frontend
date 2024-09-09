6b432af8dfe0f197c7a5774004c62895a2a70757
NurMuhammadCSE <nurmuhammadcse@gmail.com>
2024-09-09 06:27:49 +0600

    AuthProviders & Provider Added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
-import CommonLayout from "./(commonLayout)/layout";
+import { Providers } from "@/lib/providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <CommonLayout>{children}</CommonLayout>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
---
+++ src/lib/providers.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { Toaster } from "sonner";
+import { AuthProvider } from "./AuthProviders";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <NextUIProvider>
+      <NextThemesProvider attribute="class" defaultTheme="dark">
+        <Toaster></Toaster>
+        <AuthProvider>{children}</AuthProvider>
+      </NextThemesProvider>
+    </NextUIProvider>
+  );
+}
