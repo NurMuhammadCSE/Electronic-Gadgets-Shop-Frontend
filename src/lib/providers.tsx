039ad638e641a83cc9cbe1bdea832c25f4e0db10
NurMuhammadCSE <nurmuhammadcse@gmail.com>
2024-09-09 07:41:24 +0600

    Redux RTK Setup Done

--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -3,13 +3,17 @@ import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { Toaster } from "sonner";
 import { AuthProvider } from "./AuthProviders";
+import { Provider } from "react-redux";
+import { store } from "@/redux/store";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
       <NextThemesProvider attribute="class" defaultTheme="dark">
         <Toaster></Toaster>
-        <AuthProvider>{children}</AuthProvider>
+        <Provider store={store}>
+          <AuthProvider>{children}</AuthProvider>
+        </Provider>
       </NextThemesProvider>
     </NextUIProvider>
   );
