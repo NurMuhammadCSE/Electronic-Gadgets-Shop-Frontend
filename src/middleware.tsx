2176e63c133d45760b50b3639497bdf3f9fb7045
NurMuhammadCSE <nurmuhammadcse@gmail.com>
2024-09-28 13:54:55 +0600

    Cart Functionality Modify

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -16,5 +16,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/cart", "/checkout", "/dashboard/:path*"],
+  matcher: ["/checkout", "/dashboard/:path*"],
 };
